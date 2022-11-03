0423d477e4a78928ffcc64cc0c7767172f063afb
Wella01 <114139984+WellaJohnson@users.noreply.github.com>
2022-11-03 21:37:36 +0100

    End of project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WeatherApp
+
+### Samples for learners
---
+++ script/script.js
@@ -0,0 +1,73 @@
+// fetch(weatherUrl)
+//   .then((response) => response.json())
+//   .then((response) =>
+//     console.log(
+//       response.list.forEach((element) => {
+//         const clone = document.importNode(template.content, true);
+//         main.appendChild();
+//       })
+//     )
+//   );
+
+//   "https://api.openweathermap.org/data/2.5/forecast?lat=50.6333&lon=5.56667&appid=021c5879236ada602d5a9112f5c1708e&units=metric&lang=fr&cnt=7";
+
+// const template = document.querySelector("template");
+// console.log(template);
+
+// const main = document.querySelector("main");
+
+// clone.getElementById("maxTemp")
+
+const apiKey = "021c5879236ada602d5a9112f5c1708e";
+const latLiege = 50.6333;
+const lonLiege = 5.56667;
+const langage = "fr";
+const counter = 7;
+
+const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLiege}&lon=${lonLiege}&appid=${apiKey}&cnt=${counter}&lang=${langage}&units=metric`;
+console.log(weatherUrl);
+
+function fetchingDatas() {
+  return fetch(weatherUrl).then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        console.log(error);
+        throw new Error("Something went wrong - server-side");
+      });
+    }
+  });
+}
+
+async function displayDatas() {
+  const calls = (await fetchingDatas()) || [];
+  const callList = calls.list;
+  callList.forEach((call) => {
+    const templateElement = document.importNode(
+      document.querySelector("template").content,
+      true
+    );
+    templateElement.getElementById("date").textContent = call.dt_txt;
+    // templateElement.getElementById("icon").textContent = call.weather[0].icon;
+    templateElement.getElementById("maxTemp").textContent = `${Math.round(
+      call.main.temp_max
+    )} °C`;
+    templateElement.getElementById("minTemp").textContent = `${Math.round(
+      call.main.temp_min
+    )} °C`;
+    templateElement.getElementById("windSpeed").textContent = call.wind.speed;
+    templateElement.getElementById("description").textContent =
+      call.weather[0].description;
+
+    document.querySelector("main").appendChild(templateElement);
+  });
+}
+displayDatas();
+
+const splide = new Splide(".splide", {
+  type: "fade",
+  rewind: true,
+});
+
+splide.mount();
